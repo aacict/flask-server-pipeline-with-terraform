25b626281b19f559d06c9a03ae6e61ac2bc735bd
Ashish Thapa <aacict@gmail.com>
2025-08-16 04:25:13 -0600

    added ghce_pat variable taken from tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,8 @@ variable "env_name" {
 variable "solution_stack_name" {
   default = "64bit Amazon Linux 2 v4.2.3 running Docker" #we can choose the solution stack name from https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platform-history-docker.html
 }
+
+variable "ghcr_pat" {
+  type        = string
+  description = "github repo PAT"
+}
