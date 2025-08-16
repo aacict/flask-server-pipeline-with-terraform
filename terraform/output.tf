439bd52ee7d528da8a0dc80efd02599e92267de5
Ashish Thapa <aacict@gmail.com>
2025-08-16 04:27:11 -0600

    output file to show the public ip for the of the ec2 instance

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,10 @@
 # We are going to see the endpoint that leads to our flask server
-output "beanstalk_url" {
-  value       = aws_elastic_beanstalk_environment.flask_env.endpoint_url
-  description = "URL of the deployed Elastic Beanstalk environment"
+# output "beanstalk_url" {
+#   value       = aws_elastic_beanstalk_environment.flask_env.endpoint_url
+#   description = "URL of the deployed Elastic Beanstalk environment"
+# }
+
+output "appPublicIp" {
+  value       = aws_instance.flask_server.public_ip
+  description = "URL of the ec2 instance public ip"
 }
